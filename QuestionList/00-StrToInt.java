42b15e4bb7daca9aa3630bf992e3abdca5476ed0
NiGHTMARE <rsbgbn@126.com>
2019-04-23 18:55:00 +0800

    rebuild

---
+++ QuestionList/00-StrToInt.java
@@ -0,0 +1,20 @@
+/**
+ * 剑指OFFER 面试题0
+ * 把一个字符串转化为整数
+ */
+
+public class StrToInt {
+
+    public int StrToInt(String str){
+
+        if(str == null) return -1;
+        int strtoint = 0;
+        for(int i = 0; i< str.length(); i++){
+            char t = str.charAt(i);
+            if( t < '0' || t > '9') continue;
+            strtoint = strtoint * 10 + (t - '0');
+        }
+        if(str.charAt(0) == '-') return 0 - strtoint;
+        else return strtoint;
+    }
+}
